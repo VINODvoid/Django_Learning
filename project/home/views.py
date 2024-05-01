10d854cd52891ae7938d7de4dd660c3b25ec3eda
VINODvoid <iamlearner.v@gmail.com>
2024-05-01 06:09:52 +0530

    CI : templates folder added\

--- project/home/views.py
+++ project/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(req):
+    return render(req,'index.html')
+
+def success(req):
+    return HttpResponse("<h1>Success</h1>")
+    
+    
