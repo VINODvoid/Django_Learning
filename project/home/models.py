9df489f3871d7bf7f912a32a6ad629dd1ab8c95c
VINODvoid <iamlearner.v@gmail.com>
2024-05-02 12:27:02 +0530

    CI : migrations and CRUD Operations (Notion)

--- project/home/models.py
+++ project/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=20)
+    email=models.EmailField()
+    age = models.IntegerField()
+    address =models.TextField()
+    
+
+class Car(models.Model):
+    name= models.CharField(max_length=20)
+    color= models.CharField(max_length=20)
+    price= models.IntegerField()
+    model= models.CharField(max_length=20)
+    year= models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
