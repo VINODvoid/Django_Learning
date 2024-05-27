390e87f631d78043c0e1606a5c6a7bfa8503e972
VINODvoid <iamlearner.v@gmail.com>
2024-05-27 20:03:22 +0530

    CI : added tailwind css and config in setting.py

--- TryOut/django01/iron/views.py
+++ TryOut/django01/iron/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def app(req):
     return render(req,'iron/index.html')
+
+def order(req):
+    return render(req,'iron/order.html')
+    
